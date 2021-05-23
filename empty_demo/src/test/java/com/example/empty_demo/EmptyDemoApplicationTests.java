7bba0fd9fb2631d761a392cde61528738bc116bd
qpzm7903 <yx372114301@qq.com>
2021-05-23 21:39:19 +0800

    add empty spring demo

---
+++ empty_demo/src/test/java/com/example/empty_demo/EmptyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empty_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
