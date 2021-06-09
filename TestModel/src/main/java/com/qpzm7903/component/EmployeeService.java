2ca5341ececf7542719564f9f39eaa183f90e3d2
qpzm7903 <yx372114301@qq.com>
2021-06-09 08:35:26 +0800

    refactor

---
+++ TestModel/src/main/java/com/qpzm7903/component/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.qpzm7903.component;
+
+import com.qpzm7903.annotation.Component;
+
+/**
+ * @program: javaDemo
+ * @description: TODO description
+ * @author: qpzm7903
+ * @create: 2021-06-09 08:26
+ */
+
+@Component
+public class EmployeeService {
+
+}
