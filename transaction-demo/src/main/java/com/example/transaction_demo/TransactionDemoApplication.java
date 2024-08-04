46e0a8bb440b9cbbede88ef0d50c470f1d9ecda3
qpzm7903 <yx372114301@qq.com>
2024-08-04 10:11:30 +0800

    init spring transaction demo project

---
+++ transaction-demo/src/main/java/com/example/transaction_demo/TransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaction_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionDemoApplication.class, args);
+	}
+
+}
