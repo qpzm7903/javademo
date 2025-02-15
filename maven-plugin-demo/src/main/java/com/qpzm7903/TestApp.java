6073aa22abf333a47162f559f87975bb13a57d28
qpzm7903 <yx372114301@qq.com>
2025-02-15 10:39:43 +0800

    add maven plugin shaded demo

---
+++ maven-plugin-demo/src/main/java/com/qpzm7903/TestApp.java
@@ -0,0 +1,12 @@
+package com.qpzm7903;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestApp {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
